e70d06bbd74415d03f7f41cf926d3736417f9935
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-10 15:07:41 +0530

    changes

--- demo.java
+++ demo.java
@@ -107,3 +107,15 @@
 
 
 
+
+public class demo {
+
+    public static void main(String[] args) {
+        
+        for (int i= 10; i>=1; i = i-1)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
