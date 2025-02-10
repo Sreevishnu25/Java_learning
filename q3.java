06733616078ed3a4752781ec225fbeff49b48cee
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-11 01:39:16 +0530

    changes

--- q3.java
+++ q3.java
@@ -54,16 +54,101 @@
 //     }
 // }
 
+// public class q3 {
+//     public static void main(String[] args) {
+
+//         for (int i = 1; i <= 100; i++) {
+//             if (i % 3 == 0 && i % 5 == 0) {
+//                 System.out.println(i);
+
+//             }
+
+//         }
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class q3 {
+//     public static void main(String[] args) {
+
+//         Scanner scan = new Scanner(System.in);
+//         int[] marks = new int[5];
+
+//         for (int i = 0; i < 5; i++) {
+
+//             marks[i] = scan.nextInt();
+
+//         }
+
+//         for (int i = 0; i < 5; i++) {
+//             System.out.println(marks[i]);
+//         }
+
+//         scan.close();
+
+//     }  
+// } 
+
+// public class q3 {
+//     public static void main(String[] args) {
+
+//         int[] arr = new int[10];
+
+//         for (int i = 0; i < 10; i++) {
+//             arr[i] = i;
+//         }
+
+//         for (int i = 0; i < 10; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class q3 {
+//     public static void main(String[] args) {
+
+//         Scanner scan = new Scanner(System.in);
+
+//         int a = scan.nextInt();
+
+//         for (int i = 1; i <= 10; i++) {
+//           System.out.println(i+"x"+a +"=" +i*a);
+//           scan.close();
+
+//         }
+//     }
+// }
+
+
+
+import java.util.Scanner;
+
 public class q3 {
+
     public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int size = scan.nextInt();
+        int[] marks = new int[size];
 
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i);
 
-            }
-
+        for (int i = 0; i <  size; i++) {
+            marks[i] = scan.nextInt();
+            // scan.close();
         }
 
+        if (size > 0) {
+            int middleIndex = size / 2;
+            System.out.println("Middle element: " + marks[middleIndex]);
+        } else {
+            System.out.println("Array is empty.");
+        }
     }
 }
