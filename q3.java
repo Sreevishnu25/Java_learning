daaedbb5669f3e7642c31acf484de545b3b50dab
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-10 15:37:56 +0530

    changes

--- q3.java
+++ q3.java
@@ -2,9 +2,9 @@
 
 // public class q3 {
 //     public static void main(String[] args) {
-        
+
 //         Scanner scan = new Scanner(System.in);
-        
+
 //         int a = scan.nextInt();
 //         int b = scan.nextInt();
 
@@ -17,38 +17,56 @@
 //     }
 // }
 
+// public class q3 {
+//     public static void main(String[] args) {
 
+//         for (int i = 1; i <= 10; i++) {
+//             if (i % 2 == 0) {
+//                 System.out.println(i);
 
+//             }
 
+//         }
 
+//     }
+// }
 
+// public class q3 {
+//     public static void main(String[] args) {
 
+//         int count = 0;
 
+//         for (int i = 1; i <= 10; i++) {
+//             if (i % 2 == 0) {
+//                 // System.out.println("Even" + i);
 
+//             } else {
 
+//                 count = count + 1;
 
+//                 System.out.println("Odd" + i);
 
+//             }
 
+//         }
+//         System.out.println("Count:" + count);
 
+//     }
+// }
 
-
-
-
-
-  
 
 public class q3 {
     public static void main(String[] args) {
-        
-    
-        for (int i = 1; i <= 10; i++)
-        {   
-            if (i % 2 == 0){
-                System.out.println(i);
-            }
-            
-        }
 
+        
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0  && i % 5 == 0) {
+                System.out.println(i);
+
+            } 
+
+        }
         
     }
 }
