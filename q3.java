5856574e968471482b0e0e43eb2e3d76f1d23314
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-10 15:38:44 +0530

    changes

--- q3.java
+++ q3.java
@@ -54,19 +54,16 @@
 //     }
 // }
 
-
 public class q3 {
     public static void main(String[] args) {
 
-        
-
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0  && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println(i);
 
-            } 
+            }
 
         }
-        
+
     }
 }
