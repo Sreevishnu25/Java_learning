c4231c97e82de0828614d1544cc6b036d1c45796
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-10 15:19:27 +0530

    changes

---
+++ q3.java
@@ -0,0 +1,54 @@
+// import java.util.Scanner;   
+
+// public class q3 {
+//     public static void main(String[] args) {
+        
+//         Scanner scan = new Scanner(System.in);
+        
+//         int a = scan.nextInt();
+//         int b = scan.nextInt();
+
+//         for (int i = a; i <= b; i++)
+//         {
+//             System.out.println(i);
+//         }
+
+//         scan.close();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+public class q3 {
+    public static void main(String[] args) {
+        
+    
+        for (int i = 1; i <= 10; i++)
+        {   
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+            
+        }
+
+        
+    }
+}
