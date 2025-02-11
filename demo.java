951d6cb63203c0e95761a19bc6c0e86b208a1495
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-11 19:55:08 +0530

    changes

--- demo.java
+++ demo.java
@@ -191,3 +191,17 @@
 
 
 
+import java.util.Random;
+
+public class demo {
+
+    public static void main(String[] args) {
+        
+        int j =1;
+
+        while (j<=10){
+            System.out.println(j);
+            j++;
+        }
+    }
+} 
