74c72eefaa2fe2824cb705253d71a2fb33c6df52
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-11 19:52:16 +0530

    changes

--- demo.java
+++ demo.java
@@ -175,16 +175,19 @@
 
 
 
-public class demo {
+// public class demo {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        for (int j =1; j <=3; j++){
-            for (int i =1; i<=j; i++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//         for (int j = 3; j >= 1; j--){
+//             for (int i =1; i<=j; i++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
 
-    }
-}
+//     }
+// }
+
+
+
