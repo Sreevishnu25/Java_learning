f04eccd6e3e1607302d2367c0f46c35a749d458a
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-12 22:27:49 +0530

    changes

---
+++ laptop.java
@@ -0,0 +1,30 @@
+public class laptop {
+    String name = "";
+    String proc = "";
+    int ram = 0;
+    int price = 0;
+
+
+
+    public static void main(String[] args) {
+        
+
+        laptop lap1 = new laptop();
+
+        lap1.name = "hp";
+        lap1.proc = "i5";
+        lap1.ram = 8;
+        lap1.price = 500;
+
+        laptop lap2 = new laptop();
+
+        lap2.name = "lenova";
+        lap2.proc = "i7";
+        lap2.ram = 16;
+        lap2.price = 800;
+
+
+        System.out.println(lap2.price);
+
+    }
+}
