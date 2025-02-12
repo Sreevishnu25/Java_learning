bcee05f14f43a3069e27b7f11f1b53845dc42de3
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-12 17:52:29 +0530

    changes

---
+++ hotel.java
@@ -0,0 +1,14 @@
+public class hotel {
+    
+    int coffee = 20;
+    int tea = 10;
+
+    public static void main(String[] args) {
+        
+        // create an object of hotel
+        hotel server1 = new hotel();
+
+        System.out.println(server1.coffee);
+    }
+
+}
