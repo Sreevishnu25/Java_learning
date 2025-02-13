1d3631989772847eae3a29142877840a23396d1d
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-13 16:57:45 +0530

    change

---
+++ store.java
@@ -0,0 +1,63 @@
+// public class store {
+    
+//      void soap(int money){
+
+//         System.out.println(money);
+//         System.out.println("purchase soap");
+//      }
+
+//      void chocolate(int money){
+
+//         System.out.println(money);
+//         System.out.println("purchase chocolate");
+//      }
+
+//      void chips(int money){
+
+//         System.out.println(money);
+//         System.out.println("purchase chips");
+//      }
+
+
+//      public static void main(String[] args) {
+        
+//         store obj1 = new store();
+//         obj1.soap(100);
+
+//         store obj2 = new store();
+//         obj2.chocolate(200);
+
+//         store obj3 = new store();
+//         obj3.chips(300);
+
+//      }
+
+// }
+
+
+
+
+
+
+
+
+
+
+public class store {
+    
+    void sum(int a, int b){
+
+       System.out.println(a+b);
+       
+    }
+
+    
+
+    public static void main(String[] args) {
+       
+       store obj1 = new store();
+         obj1.sum(10,20);
+
+    }
+
+}
