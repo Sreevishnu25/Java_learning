06ed9f7a513e84ba7a048309a73b7b9c99f7beb7
Sreevishnu25 <spvishnu19@hotmail.com>
2025-02-27 01:17:36 +0530

    changes

---
+++ q5.java
@@ -0,0 +1,146 @@
+
+// class animal{
+// 	String name;
+// 	int age;
+	
+// 	void makesound() {
+// 		System.out.println("this animal makes sound");
+// 	}
+// }	
+
+
+// class dog extends animal{
+// 	String breed;
+	
+// 	@Override
+// 	void makesound() {
+// 		System.out.println("dog barks");
+// 	}
+	
+// 	void fetch() {
+// 		System.out.println("Dog is fetching");
+// 	}
+	
+// }
+
+// class cat extends animal{
+// 	String color;
+	
+	
+// 	@Override
+// 	void makesound() {
+// 		System.out.println("Meoww...");
+// 	}
+	
+// 	void climb() {
+// 		System.out.println("cat is climbing");
+// 	}
+// }
+
+
+// public class q5{
+// 	public static void main(String[] args) {
+// 		dog d1 = new dog();
+// 		d1.makesound();
+// 		d1.name = "timmy";
+// 		d1.age = 2;
+// 		d1.breed = "golden";
+// 		d1.fetch();
+		
+// 		cat c1 = new cat();
+// 		c1.name = "pinky";
+// 		c1.age = 3;
+// 		c1.color = "brown";
+// 		c1.makesound();
+// 		c1.climb();
+// 	}
+	
+// }
+
+
+
+// // class vehicle{
+// // 	String brand;
+// // 	int year;
+	
+// // 	void startengine() {
+// // 		System.out.println("Engine Started");
+// // 	}
+// // }
+
+// // class car extends vehicle{
+// // 	String fueltype;
+	
+// // 	@Override
+// // 	void startengine() {
+// // 		System.out.println("car engine starts");
+// // 	}
+	
+// // 	void drive() {
+// // 		System.out.println("car is driving");
+// // 	}
+	
+// // }
+
+
+// // class truck extends vehicle{
+// // 	int loadcapacity;
+	
+// // 	@Override
+// // 	void startengine() {
+// // 		System.out.println("truck engine starts");
+// // 	}
+	
+// // 	void haul() {
+// // 		System.out.println("truck is hauling");
+// // 	}
+	
+// // }
+
+
+
+// // public class q5{
+// // 	public static void main(String[] args) {
+// // 		car c1 = new car();
+// // 		c1.brand = "bmw";
+// // 		c1.year = 2002;
+// // 		c1.fueltype = "petrol";
+// // 		c1.startengine();
+// // 		c1.drive();
+// // 		System.out.println(c1.brand);
+		
+// // 		truck t1 = new truck();
+// // 		t1.brand = "tata";
+// // 		t1.year = 2005;
+// // 		t1.loadcapacity = 10;
+// // 		t1.startengine();
+// // 		t1.haul();
+// // 	}
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
